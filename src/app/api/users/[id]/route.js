93bc6beeb0d1c1a5252ec0776d0fc979a3f22e1f
navyline <150894188+navyline@users.noreply.github.com>
2024-08-31 04:33:32 +0700

    Update route.js

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -30,18 +30,18 @@ export async function DELETE(request, { params }) {
   if (res.rows.length === 0) {
   return new Response(JSON.stringify({ error: 'User not found' }), {
   status: 404,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Access-Control-Allow-Origin': '*',"Content-Type": "application/json" },
   });
   }
   return new Response(JSON.stringify(res.rows[0]), {
   status: 200,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Access-Control-Allow-Origin': '*',"Content-Type": "application/json" },
   });
   } catch (error) {
   console.error(error);
   return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
   status: 500,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Access-Control-Allow-Origin': '*',"Content-Type": "application/json" },
   });
   }
   }
